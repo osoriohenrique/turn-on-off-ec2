37472aca6c8a1082c839ba693a647a0f00d6ae06
osoriohenrique <osoriohenrique@yahoo.com.br>
2019-11-15 23:33:20 -0300

    Added functions

--- lambda_function.py
+++ lambda_function.py
@@ -1,24 +1,61 @@
 import boto3
 from datetime import datetime
-
 hour_now = datetime.now().strftime("%H:%M")
 
 ec2 = boto3.client('ec2')
 
-response = ec2.describe_instances(
-    Filters=[
-        {
-            'Name': 'tag-key',
-            'Values': ['TURNON']
-        }
-    ]
-)
+def start_instances():
+    response = ec2.describe_instances(
+        Filters=[
+            {
+                'Name': 'tag-key',
+                'Values': ['TURNON']
+            }
+        ]
+    )
 
-instanceid = ''
-for reservation in (response["Reservations"]):
-    for instance in reservation["Instances"]:
-        instanceid = instance['InstanceId']
-        for tag in instance['Tags']:
-            if tag['Key'] == 'TURNON':
-                if tag['Value'] == hour_now:
-                    print(instanceid)
+    instanceid = ''
+    for reservation in (response["Reservations"]):
+        for instance in reservation["Instances"]:
+            instanceid = instance['InstanceId']
+            for tag in instance['Tags']:
+                if tag['Key'] == 'TURNON':
+                    if tag['Value'] == hour_now:
+                        ec2.start_instances(
+                            InstanceIds=[
+                                instanceid
+                            ],
+                            DryRun=False
+                        )
+
+def stop_instances():
+    response = ec2.describe_instances(
+        Filters=[
+            {
+                'Name': 'tag-key',
+                'Values': ['TURNOFF']
+            }
+        ]
+    )
+
+    instanceid = ''
+    for reservation in (response["Reservations"]):
+        for instance in reservation["Instances"]:
+            instanceid = instance['InstanceId']
+            for tag in instance['Tags']:
+                if tag['Key'] == 'TURNOFF':
+                    if tag['Value'] == hour_now:
+                        ec2.stop_instances(
+                            InstanceIds=[
+                                instanceid
+                            ],
+                            Hibernate=False,
+                            DryRun=False,
+                            Force=False
+                        )
+
+def lambda_handler(event, context):
+    start_instances()
+    stop_instances()
+
+lambda_handler(1,1)
